b76df9773a1bc7c01ce2dee367da74a0dcd4f277
Tim Schneider <fyjham@gmail.com>
2018-08-13 19:55:05 +1000

    Build script related changes

---
+++ PathfinderSpellDB/setup/Build.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+const builder = require("electron-builder")
+const Platform = builder.Platform
+
+// Promise is returned
+builder.build({
+    "targets": Platform.WINDOWS.createTarget(),
+    "publish": "always",
+    config: {
+        'win': {
+            "publish": ["github"]
+        }
+    }
+})
+    .then(() => {
+        console.log("Installer created successfully.");
+    })
+    .catch((e) => console.log(`Error building: ${e.message}`));
