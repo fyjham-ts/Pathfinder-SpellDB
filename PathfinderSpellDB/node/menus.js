782ab9def3cf0c77d8a41457f8c3e7c0c1586bce
Tim Schneider <fyjham@gmail.com>
2018-08-22 17:24:37 +1000

    Restructured menu. Added separate menu for quick references.

--- PathfinderSpellDB/node/menus.js
+++ PathfinderSpellDB/node/menus.js
@@ -2,68 +2,84 @@
 const { autoUpdater } = require("electron-updater");
 const isDev = require('electron-is-dev');
 const updateManager = require("./updateManager");
-var menuTemplate = [
+
+var menuItems = {
+    'alwaysOnTop': {
+        'label': 'Always on top',
+        'type': 'checkbox',
+        'checked': false,
+        'click'(item, win) {
+            win.setAlwaysOnTop(item.checked);
+        }
+    },
+    'checkUpdates': {
+        'label': 'Check For Updates',
+        'click'(item, win) {
+            updateManager.openDialog();
+        }
+    },
+    'about': {
+        'label': 'About',
+        'click'(item, win) {
+            let aboutWindow = new BrowserWindow({
+                width: 600,
+                height: 400,
+                webPreferences: {
+                    nodeIntegration: true
+                },
+                autoHideMenuBar: !isDev
+            });
+            aboutWindow.loadFile('about.html');
+            aboutWindow.on('closed', function () { aboutWindow = null });
+        }
+    },
+    'quickRef': {
+        'label': 'Quick Reference',
+        'click': function (item, win) {
+            let quickRefWindow = new BrowserWindow({
+                width: 600,
+                height: 400,
+                webPreferences: {
+                    nodeIntegration: true
+                }
+            });
+            quickRefWindow.setMenu(Menu.buildFromTemplate(quickRefMenuTemplate));
+            quickRefWindow.loadFile('quickref.html');
+            quickRefWindow.on('closed', function () { quickRefWindow = null });
+        }
+    }
+};
+var quickRefMenuTemplate = [
     {
         'label': 'File',
         'submenu': [
-            {
-                'label': 'Always on top',
-                'type': 'checkbox',
-                'checked': false,
-                'click'(item, win) {
-                    win.setAlwaysOnTop(item.checked);
-                }
-            },
+            menuItems.alwaysOnTop,
+            { 'role': 'close' }
+        ]
+    }
+];
+
+var mainMenuTemplate = [
+    {
+        'label': 'File',
+        'submenu': [
+            menuItems.alwaysOnTop,
             { 'type': 'separator' },
-            {
-                'label': 'Check For Updates',
-                'click'(item, win) {
-                    updateManager.openDialog();
-                }
-            },
+            menuItems.checkUpdates,
             { 'role': 'reload' },
             { 'role': 'toggleDevTools' },
             { 'type': 'separator' },
-            {
-                'label': 'About',
-                'click'(item, win) {
-                    let aboutWindow = new BrowserWindow({
-                        width: 600,
-                        height: 400,
-                        webPreferences: {
-                            nodeIntegration: true
-                        },
-                        autoHideMenuBar: !isDev
-                    });
-                    aboutWindow.loadFile('about.html');
-                    aboutWindow.on('closed', function () { aboutWindow = null });
-                }
-            },
+            menuItems.about,
             { 'type': 'separator' },
             { 'role': 'close' }
         ]
     }, {
         'label': 'Other',
         'submenu': [
-            {
-                'label': 'Quick Reference',
-                'click': function (item, win) {
-                    let quickRefWindow = new BrowserWindow({
-                        width: 600,
-                        height: 400,
-                        webPreferences: {
-                            nodeIntegration: true
-                        },
-                        autoHideMenuBar: !isDev
-                    });
-                    quickRefWindow.setMenu(Menu.buildFromTemplate(menuTemplate));
-                    quickRefWindow.loadFile('quickref.html');
-                    quickRefWindow.on('closed', function () { quickRefWindow = null });
-                }
-            }
+            menuItems.quickRef
         ]
     }
 ];
 module.exports = {
-    mainMenuTemplate: menuTemplate
+    mainMenuTemplate: mainMenuTemplate
 };
