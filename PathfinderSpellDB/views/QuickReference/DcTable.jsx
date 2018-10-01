17a49aefa8f0a02dfc666620b088579931f80fd5
Tim Schneider <fyjham@gmail.com>
2018-10-01 15:41:12 +1000

    Incorporating updates for Erratas up to 1.3

--- PathfinderSpellDB/views/QuickReference/DcTable.jsx
+++ PathfinderSpellDB/views/QuickReference/DcTable.jsx
@@ -1,32 +1,32 @@
 ﻿var dcs = [
-    { "level": 0, "Trivial": 9, "Low": 10, "High": 12, "Severe": 14, "Extreme": 17 },
-    { "level": 1, "Trivial": 10, "Low": 12, "High": 14, "Severe": 15, "Extreme": 18 },
-    { "level": 2, "Trivial": 11, "Low": 13, "High": 15, "Severe": 16, "Extreme": 19 },
-    { "level": 3, "Trivial": 12, "Low": 15, "High": 17, "Severe": 18, "Extreme": 21 },
-    { "level": 4, "Trivial": 13, "Low": 17, "High": 19, "Severe": 20, "Extreme": 23 },
-    { "level": 5, "Trivial": 14, "Low": 18, "High": 21, "Severe": 22, "Extreme": 25 },
-    { "level": 6, "Trivial": 15, "Low": 19, "High": 22, "Severe": 23, "Extreme": 26 },
-    { "level": 7, "Trivial": 16, "Low": 20, "High": 23, "Severe": 24, "Extreme": 27 },
-    { "level": 8, "Trivial": 17, "Low": 21, "High": 24, "Severe": 26, "Extreme": 29 },
-    { "level": 9, "Trivial": 18, "Low": 23, "High": 26, "Severe": 27, "Extreme": 30 },
-    { "level": 10, "Trivial": 19, "Low": 24, "High": 27, "Severe": 29, "Extreme": 32 },
-    { "level": 11, "Trivial": 20, "Low": 25, "High": 28, "Severe": 30, "Extreme": 33 },
-    { "level": 12, "Trivial": 21, "Low": 27, "High": 30, "Severe": 32, "Extreme": 35 },
-    { "level": 13, "Trivial": 22, "Low": 29, "High": 32, "Severe": 35, "Extreme": 38 },
-    { "level": 14, "Trivial": 23, "Low": 30, "High": 33, "Severe": 36, "Extreme": 39 },
-    { "level": 15, "Trivial": 24, "Low": 32, "High": 35, "Severe": 37, "Extreme": 40 },
-    { "level": 16, "Trivial": 25, "Low": 33, "High": 36, "Severe": 39, "Extreme": 42 },
-    { "level": 17, "Trivial": 26, "Low": 35, "High": 38, "Severe": 40, "Extreme": 43 },
-    { "level": 18, "Trivial": 27, "Low": 36, "High": 39, "Severe": 41, "Extreme": 44 },
-    { "level": 19, "Trivial": 28, "Low": 37, "High": 40, "Severe": 42, "Extreme": 45 },
-    { "level": 20, "Trivial": 29, "Low": 38, "High": 41, "Severe": 44, "Extreme": 47 },
-    { "level": 21, "Trivial": 32, "Low": 42, "High": 45, "Severe": 47, "Extreme": 50 },
-    { "level": 22, "Trivial": 34, "Low": 44, "High": 47, "Severe": 49, "Extreme": 52 },
-    { "level": 23, "Trivial": 36, "Low": 46, "High": 49, "Severe": 51, "Extreme": 54 }
+    { "level":  0, "Easy":  7, "Medium": 11, "Hard": 13, "Incredible": 14, "Ultimate": 16 },
+    { "level":  1, "Easy":  8, "Medium": 13, "Hard": 15, "Incredible": 16, "Ultimate": 18 },
+    { "level":  2, "Easy":  9, "Medium": 14, "Hard": 16, "Incredible": 17, "Ultimate": 19 },
+    { "level":  3, "Easy": 10, "Medium": 15, "Hard": 17, "Incredible": 19, "Ultimate": 20 },
+    { "level":  4, "Easy": 11, "Medium": 16, "Hard": 18, "Incredible": 20, "Ultimate": 21 },
+    { "level":  5, "Easy": 12, "Medium": 18, "Hard": 20, "Incredible": 22, "Ultimate": 23 },
+    { "level":  6, "Easy": 13, "Medium": 19, "Hard": 21, "Incredible": 23, "Ultimate": 25 },
+    { "level":  7, "Easy": 14, "Medium": 21, "Hard": 22, "Incredible": 26, "Ultimate": 27 },
+    { "level":  8, "Easy": 15, "Medium": 22, "Hard": 24, "Incredible": 27, "Ultimate": 28 },
+    { "level":  9, "Easy": 16, "Medium": 23, "Hard": 26, "Incredible": 29, "Ultimate": 30 },
+    { "level": 10, "Easy": 17, "Medium": 24, "Hard": 27, "Incredible": 31, "Ultimate": 32 },
+    { "level": 11, "Easy": 18, "Medium": 25, "Hard": 28, "Incredible": 32, "Ultimate": 33 },
+    { "level": 12, "Easy": 19, "Medium": 26, "Hard": 30, "Incredible": 33, "Ultimate": 34 },
+    { "level": 13, "Easy": 20, "Medium": 28, "Hard": 32, "Incredible": 35, "Ultimate": 36 },
+    { "level": 14, "Easy": 21, "Medium": 29, "Hard": 33, "Incredible": 36, "Ultimate": 38 },
+    { "level": 15, "Easy": 22, "Medium": 30, "Hard": 35, "Incredible": 37, "Ultimate": 40 },
+    { "level": 16, "Easy": 23, "Medium": 32, "Hard": 36, "Incredible": 38, "Ultimate": 41 },
+    { "level": 17, "Easy": 24, "Medium": 33, "Hard": 38, "Incredible": 40, "Ultimate": 43 },
+    { "level": 18, "Easy": 25, "Medium": 34, "Hard": 39, "Incredible": 41, "Ultimate": 44 },
+    { "level": 19, "Easy": 26, "Medium": 35, "Hard": 40, "Incredible": 42, "Ultimate": 45 },
+    { "level": 20, "Easy": 27, "Medium": 36, "Hard": 41, "Incredible": 43, "Ultimate": 47 },
+    { "level": 21, "Easy": 28, "Medium": 38, "Hard": 45, "Incredible": 45, "Ultimate": 49 },
+    { "level": 22, "Easy": 29, "Medium": 39, "Hard": 47, "Incredible": 47, "Ultimate": 51 },
+    { "level": 23, "Easy": 30, "Medium": 41, "Hard": 49, "Incredible": 49, "Ultimate": 53 }
 ];
 import React from 'react';
 
-var difficulties = ["Trivial", "Low", "High", "Severe", "Extreme"];
+var difficulties = ["Easy", "Medium", "Hard", "Incredible", "Ultimate"];
 export default class DcTable extends React.PureComponent {
     constructor(props) {
         super(props);
