83233bd93594d98c77c61a202bfd9e9d27629dd2
Tim Schneider <fyjham@gmail.com>
2018-11-20 13:59:28 +1100

    Cleaned up some monster ability descriptions - sourced details from quick reference guides but wasn't happy with quality of data. Added icons for actions & proper heading separation on abilities.

--- PathfinderSpellDB/views/QuickReference/QuickReferenceList.jsx
+++ PathfinderSpellDB/views/QuickReference/QuickReferenceList.jsx
@@ -13,11 +13,30 @@ export default class Conditions extends React.PureComponent {
         expanded[name] = !expanded[name];
         this.setState({ expanded: expanded });
     }
+    buildIcons(actions) {
+        if (actions) {
+            return actions.split('').map(c => {
+                switch (c) {
+                    case 'f':
+                        return <img src="../images/icons/Free.png" />;
+                    case 'a':
+                        return <img src="../images/icons/action.png" />;
+                    case 'r':
+                        return <img src="../images/icons/Reaction.png" />;
+                }
+            });
+        }
+    }
     render() {
         return <div className="quickRefList accordion">
             {this.props.list.map((c) => <div key={c.name} className="card">
-                <div className="card-header" onClick={() => this.toggleExpand(c.name)}>{c.name}</div>
-                {this.state.expanded[c.name] && <div className="card-body">{c.description}</div>}
+                <div className="card-header" onClick={() => this.toggleExpand(c.name)}>{this.buildIcons(c.actions)}{c.name}</div>
+                {this.state.expanded[c.name] &&
+                    <div className="card-body">
+                        {c.description}
+                        {(c.subheadings || []).map(sh => <div className="card-subheading"><span className="card-subheading-title">{sh.title}</span>{sh.text}</div>)}
+                    </div>
+                }
             </div>)}
         </div>;
     }
